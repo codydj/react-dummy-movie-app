4aa6641536b74a699e8fdd065a4729ad3b311705
Cody Justus <codydj13@hotmail.com>
2021-11-01 17:20:26 -0600

    first commit

---
+++ build/precache-manifest.23e899d51a944c2a85d65ad60a0edf66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c123c91fa09e7ad35616c7c5c25fcdb4",
+    "url": "/react-dummy-movie-app/index.html"
+  },
+  {
+    "revision": "abf6f3fbd0946535aa1b",
+    "url": "/react-dummy-movie-app/static/css/main.280ad55c.chunk.css"
+  },
+  {
+    "revision": "073b321df77737db16f0",
+    "url": "/react-dummy-movie-app/static/js/2.ad3588c6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-dummy-movie-app/static/js/2.ad3588c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abf6f3fbd0946535aa1b",
+    "url": "/react-dummy-movie-app/static/js/main.3ad40f6b.chunk.js"
+  },
+  {
+    "revision": "e9fb99e9da83cf4ac27c",
+    "url": "/react-dummy-movie-app/static/js/runtime-main.016215c9.js"
+  }
+]);
